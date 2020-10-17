db5a553f9a2fda2936a08d4826e8853a04519426
mr-hana <owl.labo@gmail.com>
2020-10-17 23:05:35 +0900

    chore: FC利用に変更

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default (props: { children?: string }) => {
+export const Button: React.FC = ({ children }) => {
   return (
     <button className="flex justify-center py-2 px-4 border border-transparent text-sm rounded-sm text-white bg-secondary-dark hover:bg-secondary-light focus:outline-none focus:border-seconday-light focus:shadow-outline-indigo active:bg-secondary-light transition duration-150 ease-in-out">
-      {props.children}
+      {children}
     </button>
   )
 }
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default (props: { value: string }) => {
+type Props = {
+  value: string
+}
+
+export const TextField: React.FC<Props> = ({ value }) => {
   return (
     <input
-      {...props}
+      value={value}
       type="text"
       className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray placeholder-gray-light text-gray-dark focus:outline-none focus:shadow-outline-blue focus:border-secondary-dark sm:text-sm" />
   )
